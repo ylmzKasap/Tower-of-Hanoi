663655dc2641424e08ebf1d09f97da28794689c5
Yılmaz Kasap <ylmzkasap@outlook.com>
2021-01-22 09:30:11 +0300

    Converted input type from ABC to 123.

--- Tower of Hanoi.py
+++ Tower of Hanoi.py
@@ -2,12 +2,12 @@ import os
 import copy
 import sys
 
-NUMBER_OF_DISKS = 2
+NUMBER_OF_DISKS = 5
 
 disks = [i + 1 for i in range(NUMBER_OF_DISKS)]
 startingPosition = [[i, 0, 0] for i in range(NUMBER_OF_DISKS + 1)]
-possibleMoves = ['AB', 'AC', 'BA', 'BC', 'CA', 'CB']
-rodNumber = {'A': 0, 'B': 1, 'C': 2}
+possibleMoves = ['12', '13', '21', '23', '31', '32']
+rodNumber = {'1': 0, '2': 1, '3': 2}
 introduction = 0
 
 
@@ -30,7 +30,7 @@ def print_position(positionList):
         for row in rowList:
             print(str(row).center(24), end='')
         print()
-    print('A'.center(24), 'B'.center(24), 'C'.center(22), '\n')
+    print('-1-'.center(24), '-2-'.center(24), '-3-'.center(22), '\n')
 
 
 def landing_area_loop(positionList):
@@ -42,36 +42,36 @@ def landing_area_loop(positionList):
     return 0, landingArea - 1
 
 
-def get_landing_area(positionList):
-    A = [positionList[i][0] for i in range(len(positionList))]
-    B = [positionList[i][1] for i in range(len(positionList))]
-    C = [positionList[i][2] for i in range(len(positionList))]
+def get_landing_area(positionList) -> object:
+    one = [positionList[i][0] for i in range(len(positionList))]
+    two = [positionList[i][1] for i in range(len(positionList))]
+    three = [positionList[i][2] for i in range(len(positionList))]
 
-    topA, landingA = landing_area_loop(A)
-    topB, landingB = landing_area_loop(B)
-    topC, landingC = landing_area_loop(C)
+    topOne, landingOne = landing_area_loop(one)
+    topTwo, landingTwo = landing_area_loop(two)
+    topThree, landingThree = landing_area_loop(three)
 
-    return topA, topB, topC, landingA, landingB, landingC
+    return topOne, topTwo, topThree, landingOne, landingTwo, landingThree
 
 
 def player_move(move, positionList):
-    topA, topB, topC, landingA, landingB, landingC = get_landing_area(positionList)
-    topDisks = {'A': topA, 'B': topB, 'C': topC}
-    landingArea = {'A': landingA, 'B': landingB, 'C': landingC}
+    topOne, topTwo, topThree, landingOne, landingTwo, landingThree = get_landing_area(positionList)
+    topDisks = {'1': topOne, '2': topTwo, '3': topThree}
+    landingArea = {'1': landingOne, '2': landingTwo, '3': landingThree}
 
     if len(move) == 1:
-        if landingB == 0 or landingC == 0:
+        if landingTwo == 0 or landingThree == 0:
             return 'win'
         return
 
     if topDisks[move[0]] == 0:
-        print('\n' + f'There is no disk to move in {move[0]}.'.center(75))
+        print('\n' + f'There is no disk to move in -{move[0]}-'.center(75))
         return
     elif topDisks[move[1]] != 0:
         if topDisks[move[0]] > topDisks[move[1]]:
             print(
-                '\n' + (f'Incorrect move, top disk in {move[0]}({topDisks[move[0]]})'
-                        + f' is larger than {move[1]}({topDisks[move[1]]}).').center(75))
+                '\n' + (f'Incorrect move, top disk in -{move[0]}- ({topDisks[move[0]]})'
+                        + f' is larger than -{move[1]}- ({topDisks[move[1]]}).').center(75))
             return
     elif topDisks[move[0]] == topDisks[move[1]]:
         print('\n' + 'Incorrect move, both rods are empty.'.center(75))
@@ -87,7 +87,7 @@ def let_the_game_begin(positionList):
     global introduction
     if introduction == 0:
         print('\n' + 'Move the entire stack of disks to another rod to win the game.'.center(75))
-        print("Sample move: Typing 'AB' moves the top disk on rod A to rod B.".center(75))
+        print("Sample move: Typing '12' moves the top disk on rod -1- to rod -2-.".center(75))
         introduction = 1
     moveNumber = 0
 
@@ -117,7 +117,7 @@ def let_the_game_begin(positionList):
         print_position(positionList)
         print('\n' + f'Congratulations! You won in {moveNumber} moves.'.center(75))
         print('\n' + 'Press \'a\' to play again, \'q\' to exit.'.center(75))
-        decision = input()
+        decision = input().lower()
         if decision == 'a':
             os.system('cls')
             let_the_game_begin(copy.deepcopy(startingPosition))
