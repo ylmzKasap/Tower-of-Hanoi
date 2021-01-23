648ae38fc17c7ac7c1ebd671fd09e2a4d0322967
Yılmaz Kasap <ylmzkasap@outlook.com>
2021-01-23 14:16:52 +0300

    Added docstring.

--- towerOfHanoi.py
+++ towerOfHanoi.py
@@ -12,37 +12,36 @@ introduction = 0
 
 
 def format_position(positionList):
-    positionListRaw = copy.deepcopy(positionList)
-    for i, item in enumerate(positionListRaw):
+    """Takes a numeric list and turns zeros into rods (||).
+    It also formats each disk with '@'."""
+
+    formattedList = copy.deepcopy(positionList)
+    for i, item in enumerate(formattedList):
         for ii, subItem in enumerate(item):
             if subItem == 0:
-                positionListRaw[i][ii] = '||'
+                formattedList[i][ii] = '||'
             else:
-                positionListRaw[i][ii] = f'_{subItem}'.center((subItem * 2) + 2, '@')
-    return positionListRaw
+                formattedList[i][ii] = f'_{subItem}'.center((subItem * 2) + 2, '@')
+    return formattedList
 
 
 def print_position(positionList):
-    positionList = format_position(positionList)
+    """Takes a formatted list and prints it by aligning the rods."""
+
+    formattedList = format_position(positionList)
 
     print()
-    for rowList in positionList:
+    for rowList in formattedList:
         for row in rowList:
             print(str(row).center(24), end='')
         print()
     print('-1-'.center(24), '-2-'.center(24), '-3-'.center(22), '\n')
 
 
-def landing_area_loop(positionList):
-    landingArea = 0
-    for row in positionList:
-        if row != 0:
-            return row, landingArea - 1
-        landingArea += 1
-    return 0, landingArea - 1
+def get_landing_area(positionList):
+    """Takes a numeric list and returns the the length of top disks
+    and the index of landing area in each rod."""
 
-
-def get_landing_area(positionList) -> object:
     one = [positionList[i][0] for i in range(len(positionList))]
     two = [positionList[i][1] for i in range(len(positionList))]
     three = [positionList[i][2] for i in range(len(positionList))]
@@ -54,7 +53,20 @@ def get_landing_area(positionList) -> object:
     return topOne, topTwo, topThree, landingOne, landingTwo, landingThree
 
 
+def landing_area_loop(positionList):
+    """A loop to shorten get_landing_area function."""
+
+    landingArea = 0
+    for row in positionList:
+        if row != 0:
+            return row, landingArea - 1
+        landingArea += 1
+    return 0, landingArea - 1
+
+
 def player_move(move, positionList):
+    """Checks whether the submitted move is valid and alters the position list."""
+
     topOne, topTwo, topThree, landingOne, landingTwo, landingThree = get_landing_area(positionList)
     topDisks = {'1': topOne, '2': topTwo, '3': topThree}
     landingArea = {'1': landingOne, '2': landingTwo, '3': landingThree}
@@ -64,20 +76,22 @@ def player_move(move, positionList):
             return 'win'
         return
 
-    if topDisks[move[0]] == 0:
+    topDisk1, topDisk2 = topDisks[move[0]], topDisks[move[1]]
+
+    if topDisk1 == 0:
         print('\n' + f'There is no disk to move in -{move[0]}-'.center(75))
         return
-    elif topDisks[move[1]] != 0:
-        if topDisks[move[0]] > topDisks[move[1]]:
+    elif topDisk2 != 0:
+        if topDisk1 > topDisk2:
             print(
-                '\n' + (f'Incorrect move, top disk in -{move[0]}- ({topDisks[move[0]]})'
-                        + f' is larger than -{move[1]}- ({topDisks[move[1]]}).').center(75))
+                '\n' + (f"Incorrect move, {'@' * topDisk1}_{topDisk1}{'@' * topDisk1}"
+                        + f" is larger than {'@' * topDisk2}_{topDisk2}{'@' * topDisk2}.").center(75))
             return
-    elif topDisks[move[0]] == topDisks[move[1]]:
+    elif topDisk1 == topDisk2:
         print('\n' + 'Incorrect move, both rods are empty.'.center(75))
         return
 
-    positionList[landingArea[move[1]]][rodNumber[move[1]]] = topDisks[move[0]]
+    positionList[landingArea[move[1]]][rodNumber[move[1]]] = topDisk1
     positionList[landingArea[move[0]] + 1][rodNumber[move[0]]] = 0
     return positionList
 
@@ -91,7 +105,7 @@ def let_the_game_begin(positionList):
         introduction = 1
     moveNumber = 0
 
-    while True:
+    while True:  # Game loop. Each iteration increases the moveNumber by one.
         print('\n' + 'Enter a move.'.center(75) + '\n')
         print_position(positionList)
 
@@ -111,7 +125,7 @@ def let_the_game_begin(positionList):
         moveNumber += 1
         positionList = moveResult
 
-    while True:
+    while True:  # Enter a decision to exit win loop.
         os.system('cls')
         print()
         print_position(positionList)
